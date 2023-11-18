a8ba3ff74cd782bd6fa62554775663af4f99ea76
Aidan Timson <aidan@timmo.dev>
2023-11-18 12:35:24 +0000

    Upgrade python syntax (#3)
    * Upgrade python syntax
    
    * Fix

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 """Setup"""
-import io
 import json
 import os
-from distutils.dir_util import copy_tree
+from shutil import copytree
 
 from setuptools import find_packages, setup
 
 # Get setup packages from requirements.txt
-with io.open("requirements_setup.txt", encoding="utf-8") as f:
+with open("requirements_setup.txt", encoding="utf-8") as f:
     requirements_setup = f.read().splitlines()
 
 # Get packages from requirements.txt
-with io.open("requirements.txt", encoding="utf-8") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-copy_tree(
+copytree(
     "WindowsSensors/bin/net7.0-windows",
     "systembridgewindowssensors/bin",
+    dirs_exist_ok=True,
 )
 
 package_data = []
